f64deb02952745a7cae4caba076d1b4cae53d68e
Cláudio Más <claudio.mas@gmail.com>
2024-06-14 16:43:34 -0300

    correção de baloons

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if uploaded_file is not None:
 
     st.success("Arquivo processado com sucesso! Clique no botão abaixo para converter em .xml")
 
-    st.balloons
+    st.balloons()
 
     st.download_button(
         label="Converter para XML",
